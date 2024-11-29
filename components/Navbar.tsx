56b0c1282581fc9f713630f70cccc213fe06351c
NILAVABEPARI <nilavastnd@gmail.com>
2024-11-29 09:31:25 +0530

    First commit -- completed authentication, hero section, search form and startup cards

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { auth, signIn, signOut } from '@/auth';
+
+const Navbar = async () => {
+    const session = await auth();
+    // console.log("auth -> ", auth());
+    console.log("session -> ", session);
+
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href="/">
+                    <Image src="/logo.png" alt='logo' width={144} height={30} />
+                </Link>
+
+                <div className='flex items-center gap-5 text-black'>
+                    {
+                        session && session?.user ? (
+                            <>
+                                <Link href="/startup/create">
+                                    <span>Create</span>
+                                </Link>
+
+                                <form action={async () => {
+                                    "use server";
+                                    await signOut({ redirectTo: "/" });
+                                }}>
+                                    <button type='submit'>Logout</button>
+                                </form>
+                                <Link href={`/user/${session?.user?.email}`}>
+                                    <span>{session?.user?.name}</span>
+                                </Link>
+                            </>
+                        ) :
+                            (
+                                <form action={async () => {
+                                    "use server";
+                                    await signIn('github')
+                                }}>
+                                    <button type='submit'>
+                                        <span>Login</span>
+                                    </button>
+                                </form>
+                            )
+                    }
+                </div>
+            </nav>
+        </header >
+    )
+}
+
+export default Navbar
